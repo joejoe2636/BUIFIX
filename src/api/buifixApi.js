344be4dd0dfebdc46a9f9ca8b0d45e5f9a8ed269
devemmm <emmanuel@gennxttechsol.com>
2023-01-18 11:25:52 +0200

    changing server ip add

--- src/api/buifixApi.js
+++ src/api/buifixApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://38.242.156.44:3000',
+    baseURL: 'http://185.193.67.94:9000',
     // baseURL: 'https://buifix-api.herokuapp.com'
     // baseURL: 'https://4df7-105-178-110-233.eu.ngrok.io',
     // baseURL: 'http://localhost:3000'
